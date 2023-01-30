d2f785c0698ea3d0bb518c846f98fea73e25858f
please-help-me-find-the-truth <121876348+please-help-me-find-the-truth@users.noreply.github.com>
2023-01-30 13:30:40 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var video = document.querySelector("#videoElement");
 if (navigator.mediaDevices.getUserMedia) {
   navigator.mediaDevices.getUserMedia({ video: true,
     video: {
-      facingMode: 'environment'
+      facingMode: 'Environment'
     } })
     .then(function (stream) {
       video.srcObject = stream;
@@ -13,7 +13,6 @@ if (navigator.mediaDevices.getUserMedia) {
       console.log("Something went wrong!");
     });
 }
-
 
 root = document.documentElement;
 
@@ -25,38 +24,19 @@ function setRandomScaleAndPosition() {
     // calculate values
     var x = Math.floor(Math.random() * 100 - 50) // min = 0*100-50 => -50 ; max = 1*100-50 => 50
     var y = Math.floor(Math.random() * 100 - 50)
-    var scale = 3 // Math.floor(Math.random() * 6 + 2)    //siehe css var scale 
-    
+    var z = Math.floor(Math.random() * 100 - 50)
+    var scale = Math.floor(Math.random() * 6 + 2)    //siehe css var scale 
+    // var scale = 2 
     // display values in console
-    console.log("new values:", x, y, scale)
+    console.log("new values:", x, y, z, scale)
     
     // set CSS variables
   root.style.setProperty('--translate-x', x + "%");
   root.style.setProperty('--translate-y', y + "%");
+  root.style.setProperty('--translate-z', z + "%");
   root.style.setProperty('--scale', scale);
 }
 
 // run setRandomScaleAndPosition every 6 seconds
-setInterval(setRandomScaleAndPosition, 6000)
+setInterval(setRandomScaleAndPosition, 600);
 
-
-
-
-// alte version:
-// function setRandomScaleAndPosition() {
-//   // calculate values
-//   var x = Math.floor(Math.random() * 100 - 50) // min = 0*100-50 => -50 ; max = 1*100-50 => 50
-//   var y = Math.floor(Math.random() * 100 - 50)
-//   var scale = 8 // Math.floor(Math.random() * 6 + 2)    //siehe css var scale 
-  
-//   // display values in console
-//   console.log("new values:", x, y, scale)
-  
-//   // set CSS variables
-// root.style.setProperty('--translate-x', x + "%");
-// root.style.setProperty('--translate-y', y + "%");
-// root.style.setProperty('--scale', scale);
-// }
-
-// // run setRandomScaleAndPosition every 6 seconds
-// setInterval(setRandomScaleAndPosition, 6000)
